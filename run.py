3b4b06f85e68dc9f5cfa83bfd71c1cc29667b2f0
MarMarAgain <>
2024-02-04 20:26:49 +0000

    Add sales sata to sales worksheet

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def get_sales_data():
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Data is valid!")
+            print("Data is valid")
             break
 
     return sales_data
@@ -37,7 +37,7 @@ def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 6 values.Loop added to repeatedly request data until valid input is given
     """
     try:
         [int(value) for value in values]
@@ -52,4 +52,14 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet---\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated sucessfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
